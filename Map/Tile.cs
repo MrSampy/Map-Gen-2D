90d5f371c1ff41cd85caa68ce2f0384bb8e5a616
MrSampy <mynameisserzheo@gmail.com>
2022-04-23 18:06:43 +0300

    Add class `Tile`
    Add constructor in class
    Add properties: `X`, `Y`, `HeightValue`
    Add variables `_Biome`, `LeftTile`, `RightTile`, `TopTile`, `BottomTile`

---
+++ Map/Tile.cs
@@ -0,0 +1,36 @@
+using Course_work.Map.Helpers;
+namespace Course_work.Map;
+
+public class Tile
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public double HeightValue { get; set; }
+    public TilesBiomes _Biome;
+    public Tile? LeftTile;
+    public Tile? RightTile;
+    public Tile? TopTile;
+    public Tile? BottomTile;
+    public Tile(int x, int y, double heightvalue)
+    {
+        X = x;
+        Y = y;
+        HeightValue = heightvalue;
+        if (heightvalue < 0.42)
+            _Biome = new TilesBiomes(Enums.Biome.DEEPWATER,0,60,255);
+        else if (heightvalue <= 0.5)
+            _Biome = new TilesBiomes(Enums.Biome.SHALLOWWATER,53,195,255);
+        else if (heightvalue <= 0.52)
+            _Biome = new TilesBiomes(Enums.Biome.SAND,249,221,84);
+        else if (heightvalue <= 0.59)
+            _Biome = new TilesBiomes(Enums.Biome.GRASS,57,205,72);
+        else if (heightvalue <= 0.64)
+            _Biome = new TilesBiomes(Enums.Biome.FOREST,2,137,15);
+        else if (heightvalue <= 0.68)
+            _Biome = new TilesBiomes(Enums.Biome.ROCK,191,185,183);
+        else
+            _Biome = new TilesBiomes(Enums.Biome.SNOW,236,256,238);
+        
+    }
+
+}
