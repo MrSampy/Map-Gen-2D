79da427b4cb3320ef3010edcc2c14edb2b19f7e9
MrSampy <mynameisserzheo@gmail.com>
2022-04-23 18:21:31 +0300

    Add class `MapGenerator`
    - Add constructor in class
    - Add properties: `Seed`, `Width`, `Height`
    - Add array`Tiles`
    - Add method `FindNeighbours`

---
+++ Map/MapGenerator.cs
@@ -0,0 +1,49 @@
+using Course_work.Map.Helpers;
+namespace Course_work.Map;
+
+public class MapGenerator
+{
+    private Random Rnd = new Random();
+    private int Seed { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Tile[,] Tiles;
+    public MapGenerator(int width, int height)
+    {   
+        Width = width;
+        Height = height;
+        Tiles = new Tile[Width,Height];
+        Seed = Rnd.Next(1,1000);
+        PerlinNoise perlinNoise = new PerlinNoise(Seed);
+        double widthDivisor = 1 / (double)Width;
+        double heightDivisor = 1 / (double)Height;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                double tempheight =
+                    (perlinNoise.Noise(2 * x* widthDivisor, 2 * y * heightDivisor, -0.5) + 1) / 2 * 0.8 +
+                    (perlinNoise.Noise(4 * x * widthDivisor, 4 * y * heightDivisor, 0) + 1) / 2 * 0.2 +
+                    (perlinNoise.Noise(8 * x * widthDivisor, 8 * y * heightDivisor, +0.5) + 1) / 2 * 0.1;
+                tempheight = Math.Min(1, Math.Max(0, tempheight));
+                tempheight = Math.Round(tempheight,3);
+                Tiles[x, y] = new Tile(x,y,tempheight);
+            }
+        }
+        FindNeighbours();
+    }
+
+    private void FindNeighbours()
+    {
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {   
+                Tiles[x, y].LeftTile = (y-1<0) ? null : Tiles[x, y-1];
+                Tiles[x, y].RightTile = (y + 1 >= Height) ? null : Tiles[x, y+1];
+                Tiles[x, y].TopTile = (x-1<0) ? null : Tiles[x-1, y];
+                Tiles[x, y].BottomTile = (x + 1 >= Width) ? null : Tiles[x+1,y];
+            }
+        }
+    }
+}
--- Map/Tile.cs
+++ Map/Tile.cs
@@ -18,19 +18,19 @@ public class Tile
         HeightValue = heightvalue;
         if (heightvalue < 0.42)
             _Biome = new TilesBiomes(Enums.Biome.DEEPWATER,0,60,255);
-        else if (heightvalue <= 0.5)
-            _Biome = new TilesBiomes(Enums.Biome.SHALLOWWATER,53,195,255);
         else if (heightvalue <= 0.52)
+            _Biome = new TilesBiomes(Enums.Biome.SHALLOWWATER,53,195,255);
+        else if (heightvalue <= 0.53)
             _Biome = new TilesBiomes(Enums.Biome.SAND,249,221,84);
         else if (heightvalue <= 0.59)
             _Biome = new TilesBiomes(Enums.Biome.GRASS,57,205,72);
         else if (heightvalue <= 0.64)
             _Biome = new TilesBiomes(Enums.Biome.FOREST,2,137,15);
-        else if (heightvalue <= 0.68)
+        else if (heightvalue <= 0.69)
             _Biome = new TilesBiomes(Enums.Biome.ROCK,191,185,183);
         else
             _Biome = new TilesBiomes(Enums.Biome.SNOW,236,256,238);
         
-    }
+    }   
 
 }
