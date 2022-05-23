8d527d2b7d506344e6988754c91add0a5e43c7bb
MrSampy <mynameisserzheo@gmail.com>
2022-05-23 14:07:49 +0300

    Add ability to download maps in .png file

--- src/CourseWork/wwwroot/js/site.js
+++ src/CourseWork/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿let functionarr = [];
-const canvs = [`canvas1`,`canvas2`,`canvas3`];
+const canvs = ["canvas1","canvas2","canvas3`"];
 function drawRect(x, y, red, green, blue, lenofpix,num) {
     let canvas = document.getElementById(canvs[num]);
     if (canvas.getContext) {
@@ -8,10 +8,27 @@ function drawRect(x, y, red, green, blue, lenofpix,num) {
         ctx.fillRect(x, y, lenofpix, lenofpix);
     }
 }
-
-
 function drawmap() {
     for (const func of functionarr)
         func();
-    
+
+}
+function getImage(canvas){
+    var imageData = canvas.toDataURL();
+    var image = new Image();
+    image.src = imageData;
+    return image;
+}
+
+function saveImage(image) {
+    var link = document.createElement("a");
+
+    link.setAttribute("href", image.src);
+    link.setAttribute("download", "123");
+    link.click();
+}
+
+function saveCanvasAsImageFile(num){
+    var image = getImage(document.getElementById(canvs[num]));
+    saveImage(image);
 }
